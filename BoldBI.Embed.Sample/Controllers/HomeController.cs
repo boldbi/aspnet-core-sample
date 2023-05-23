dc931945eeb75e7fe81f62d76509194d99f3f74c
Nithya <nithya.gopal@syncfusion.com>
2023-05-23 08:44:46 +0530

    UMP-23843-Migrate-to-ASPNET-Core-2.1-Projects-to-ASPDOTNET-6.0

--- BoldBI.Embed.Sample/Controllers/HomeController.cs
+++ BoldBI.Embed.Sample/Controllers/HomeController.cs
@@ -26,43 +26,6 @@ namespace BoldBI.Embed.Sample.Controllers
             }
         }
 
-        [HttpGet]
-        [Route("GetDashboards")]
-        public string GetDashboards()
-        {
-            var token = GetToken();
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(GlobalAppSettings.EmbedDetails.ServerUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Add("Authorization", token.TokenType + " " + token.AccessToken);
-                var result = client.GetAsync(GlobalAppSettings.EmbedDetails.ServerUrl + "/api/" + GlobalAppSettings.EmbedDetails.SiteIdentifier + "/v2.0/items?ItemType=2").Result;
-                string resultContent = result.Content.ReadAsStringAsync().Result;
-                return resultContent;
-            }
-        }
-
-        public Token GetToken()
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(GlobalAppSettings.EmbedDetails.ServerUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-
-                var content = new FormUrlEncodedContent(new[]
-                {
-                    new KeyValuePair<string, string>("grant_type", "embed_secret"),
-                    new KeyValuePair<string, string>("Username", GlobalAppSettings.EmbedDetails.UserEmail),
-                    new KeyValuePair<string, string>("embed_secret", GlobalAppSettings.EmbedDetails.EmbedSecret)
-                });
-                var result = client.PostAsync(GlobalAppSettings.EmbedDetails.ServerUrl + "/api/" + GlobalAppSettings.EmbedDetails.SiteIdentifier + "/token", content).Result;
-                string resultContent = result.Content.ReadAsStringAsync().Result;
-                var response = JsonConvert.DeserializeObject<Token>(resultContent);
-                return response;
-            }
-        }
-
         [HttpPost]
         [Route("AuthorizationServer")]
         public string AuthorizationServer([FromBody] object embedQuerString)
